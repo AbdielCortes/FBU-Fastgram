0d79bf8df21c5168da9dede20f1a21b52396598b
Abdiel Cortes <zurken@fb.com>
2020-07-06 15:32:12 -0700

    Initial Commit

---
+++ Fastgram/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Fastgram
+//
+//  Created by zurken on 7/6/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FastgramTests/FastgramTests.m
@@ -0,0 +1,37 @@
+//
+//  FastgramTests.m
+//  FastgramTests
+//
+//  Created by zurken on 7/6/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FastgramTests : XCTestCase
+
+@end
+
+@implementation FastgramTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

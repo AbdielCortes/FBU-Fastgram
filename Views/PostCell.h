9238ae36a2bc5967101845f8ac2c907946c5f254
Abdiel Cortes <zurken@fb.com>
2020-07-07 15:17:58 -0700

    configured storyboard for home feed and capture

---
+++ Views/PostCell.h
@@ -0,0 +1,17 @@
+//
+//  PostCell.h
+//  Fastgram
+//
+//  Created by zurken on 7/7/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Views/PostCell.m
@@ -0,0 +1,24 @@
+//
+//  PostCell.m
+//  Fastgram
+//
+//  Created by zurken on 7/7/20.
+//  Copyright © 2020 FacebookUniversity. All rights reserved.
+//
+
+#import "PostCell.h"
+
+@implementation PostCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
